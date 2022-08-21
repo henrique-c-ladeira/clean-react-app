7958ad60294e61d7ffec05480bf31bb44d2c9caf
Henrique <henrique.c.ladeira@gmail.com>
2022-08-21 16:48:54 -0300

    chore: add root directory

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,12 @@
 /* eslint-disable no-undef */
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
+  roots: ['<rootDir>/src'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.{ts,tsx}'],
+  coverageDirectory: 'coverage',
   preset: 'ts-jest',
   testEnvironment: 'node',
+  moduleNameMapper: {
+    '~/(.*)': '<rootDir>/src/$1',
+  },
 };
