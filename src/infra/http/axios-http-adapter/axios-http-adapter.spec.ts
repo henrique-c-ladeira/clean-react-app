f97f27a28a17e21ecb37c526859797736b76817a
Henrique <henrique.c.ladeira@gmail.com>
2022-08-21 19:04:54 -0300

    refactor: improve structure

--- src/infra/http/axios-http-adapter/axios-http-adapter.spec.ts
+++ src/infra/http/axios-http-adapter/axios-http-adapter.spec.ts
@@ -1,42 +1,33 @@
 import { AxiosHttpAdapter } from './axios-http-adapter';
-import { HttpPostParams } from '~/data/protocols/http';
+import { mockAxiosPost } from '~/infra/test';
+import { mockPostRequest } from '~/data/test';
 import axios from 'axios';
-import { faker } from '@faker-js/faker';
 
 jest.mock('axios');
-const axiosMock = axios as jest.Mocked<typeof axios>;
-const axiosResultMock = {
-  data: JSON.parse(faker.datatype.json()),
-  status: faker.random.numeric(),
+
+type SutTypes = { sut: AxiosHttpAdapter; axiosMock: jest.Mocked<typeof axios> };
+
+const makeSut = (): SutTypes => {
+  const sut = new AxiosHttpAdapter();
+  const axiosMock = mockAxiosPost();
+  return { sut, axiosMock };
 };
-axiosMock.post.mockImplementation(() => Promise.resolve(axiosResultMock));
-
-type SutTypes = AxiosHttpAdapter;
-
-const makeSut = (): SutTypes => new AxiosHttpAdapter();
-
-const mockPostRequest = (): HttpPostParams<unknown> => ({
-  url: faker.internet.url(),
-  body: JSON.parse(faker.datatype.json()),
-});
 
 describe('AxiosHttpAdapter', () => {
   beforeEach(() => {
     jest.clearAllMocks();
   });
+
   it('should call axios with correct values', async () => {
     const request = mockPostRequest();
-    const sut = makeSut();
+    const { sut, axiosMock } = makeSut();
     sut.post(request);
     expect(axiosMock.post).toHaveBeenCalledWith(request.url, request.body);
   });
 
   it('should return correct statusCode and body', async () => {
-    const sut = makeSut();
-    const httpResponse = await sut.post(mockPostRequest());
-    expect(httpResponse).toEqual({
-      statusCode: axiosResultMock.status,
-      body: axiosResultMock.data,
-    });
+    const { sut, axiosMock } = makeSut();
+    const promise = sut.post(mockPostRequest());
+    expect(promise).toEqual(axiosMock.post.mock.results[0].value);
   });
 });
