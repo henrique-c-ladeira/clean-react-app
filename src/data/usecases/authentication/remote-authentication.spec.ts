c88219ea5a7e88ca26e946ebcf43b3f4b744ae04
Henrique <henrique.c.ladeira@gmail.com>
2022-08-21 16:38:21 -0300

    feat: add body, auth params and mocks

--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -1,6 +1,7 @@
 import { HttpPostClientSpy } from '../../test/mock-http-client';
 import { RemoteAuthentication } from './remote-authentication';
 import { faker } from '@faker-js/faker';
+import { mockAuthenticationParams } from '../../../domain/test/mock-authentication';
 
 type SutTypes = {
   sut: RemoteAuthentication;
@@ -17,7 +18,14 @@ describe('RemoteAuthentication', () => {
   it('should call http client with correct url', () => {
     const url = faker.internet.url();
     const { sut, httpPostClientSpy } = makeSut(url);
-    sut.auth();
+    sut.auth(mockAuthenticationParams());
     expect(httpPostClientSpy.url).toBe(url);
+  });
+
+  it('should call http client with correct body', () => {
+    const { sut, httpPostClientSpy } = makeSut();
+    const authenticationParams = mockAuthenticationParams();
+    sut.auth(authenticationParams);
+    expect(httpPostClientSpy.body).toEqual(authenticationParams);
   });
 });
