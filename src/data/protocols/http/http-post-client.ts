2fe6448ac26ef1170ea5c675db8f83d5a55b2043
Henrique <henrique.c.ladeira@gmail.com>
2022-08-21 17:24:12 -0300

    fix: type in body

--- src/data/protocols/http/http-post-client.ts
+++ src/data/protocols/http/http-post-client.ts
@@ -2,7 +2,7 @@ import { HttpResponse } from './http-response';
 
 export type HttpPostParams = {
   url: string;
-  body?: number;
+  body?: object;
 };
 export interface HttpPostClient {
   post(params: HttpPostParams): Promise<HttpResponse>;
