6c4c6cd4ea960838bcab6e1dc5e448c56f2936c4
Henrique <henrique.c.ladeira@gmail.com>
2022-08-28 16:16:53 -0300

    test: add first test for login

---
+++ src/presentation/modules/user-account/login-screen.spec.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render } from '~/shared/tests-utils';
+import { LoginScreen } from '.';
+
+describe('LoginScreen Component', () => {
+  it('should render', () => {
+    render(<LoginScreen />);
+  });
+});
---
+++ src/shared/tests-utils.tsx
@@ -0,0 +1,6 @@
+import { render as TLRender } from '@testing-library/react';
+import { ThemeProvider } from 'styled-components';
+import { theme } from '~/presentation/theme';
+
+export const render = (component: JSX.Element) =>
+  TLRender(<ThemeProvider theme={theme}>{component}</ThemeProvider>);
