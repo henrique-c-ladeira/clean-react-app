4d41272b8682d47979c3ceca06d5051470db2e9f
Henrique <henrique.c.ladeira@gmail.com>
2022-09-10 16:48:33 -0300

    feat: add authentication to login

--- src/main/factories/modules/user-account/login-factory.tsx
+++ src/main/factories/modules/user-account/login-factory.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import Login from '~/presentation/modules/user-account/login-screen';
+import { RemoteAuthentication } from '~/data/usecases/authentication/remote-authentication';
+import { AxiosHttpAdapter } from '~/infra/http/axios-http-adapter/axios-http-adapter';
+import Login from '~/presentation/views/user-account/login-screen';
 import { ValidationBuilder } from '~/validation/builder/validation-builder';
 import { ValidationComposite } from '~/validation/validators';
 
@@ -8,5 +10,10 @@ export const MakeLogin: React.FC = () => {
     ...ValidationBuilder.field('email').required().email().build(),
     ...ValidationBuilder.field('password').required().min(5).build(),
   ]);
-  return <Login validation={validationComposite} />;
+  const url = 'https://blooming-bayou-30676.herokuapp.com/token';
+  const httpClient = new AxiosHttpAdapter();
+  const authentication = new RemoteAuthentication(url, httpClient);
+  return (
+    <Login validation={validationComposite} authentication={authentication} />
+  );
 };
