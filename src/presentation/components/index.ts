2cc9d43f43318f78686a81d01ade25a0cc95fd00
Henrique <henrique.c.ladeira@gmail.com>
2022-09-11 17:41:06 -0300

    feat: add switch component

--- src/presentation/components/index.ts
+++ src/presentation/components/index.ts
@@ -4,3 +4,4 @@ export { default as Input } from './input/input';
 export { default as BoxContent } from './box-content/box-content';
 export { default as Button } from './button/button';
 export { default as FallbackWrapper } from './fallback-wrapper/fallback-wrapper';
+export { default as Switch } from './switch/switch';
