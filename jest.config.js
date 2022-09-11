587bf902aa13509cde0e66cbc5e796214b60b8a4
Henrique <henrique.c.ladeira@gmail.com>
2022-09-11 17:39:44 -0300

    remove coverage from entry.ts

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
   roots: ['<rootDir>/src'],
-  collectCoverageFrom: ['<rootDir>/src/**/*.{ts,tsx}', '!**/*.d.ts'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.{ts,tsx}', '!<rootDir>/src/main/entry.tsx', '!**/*.d.ts'],
   coverageDirectory: 'coverage',
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
