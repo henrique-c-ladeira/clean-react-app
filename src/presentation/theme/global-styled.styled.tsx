8df8c23a0d5cb5257dd8d9960b428e24f7fb7a22
Henrique <henrique.c.ladeira@gmail.com>
2022-09-11 17:40:40 -0300

    feat: reset style for button

--- src/presentation/theme/global-styled.styled.tsx
+++ src/presentation/theme/global-styled.styled.tsx
@@ -8,6 +8,18 @@ const GlobalStyle = createGlobalStyle`
       border: none;
     }
   }
+  button {
+    border: none;
+    margin: 0;
+    padding: 0;
+    width: auto;
+    overflow: visible;
+    background: transparent;
+    color: inherit;
+    font: inherit;
+    line-height: normal;
+  }
+
   body {
     margin: 0;
     padding: 0;
