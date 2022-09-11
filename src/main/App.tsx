24ea5e2def45ebb3123af4ce48fa008a256cbe8c
Henrique <henrique.c.ladeira@gmail.com>
2022-09-11 17:42:55 -0300

    feat: refactor and general improves

--- src/main/App.tsx
+++ src/main/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { ThemeProvider } from 'styled-components';
 import { Router } from '~/presentation/components';
 import { GlobalStyle, theme } from '~/presentation/theme';
-import { MakeLogin } from './factories/modules/user-account/login-factory';
+import { MakeLogin } from './factories/views/user-account/login-factory';
 
 export const App: React.FC = () => (
   <ThemeProvider theme={theme}>
